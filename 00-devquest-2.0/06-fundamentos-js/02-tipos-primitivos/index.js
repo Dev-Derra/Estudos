440d5c325dad24a4a180aeba7db053ec231e93cb
Gabriel <galighiti@gmail.com>
2025-11-25 13:57:27 -0300

    feat: aula sobre tipos primitivos

---
+++ 00-devquest-2.0/06-fundamentos-js/02-tipos-primitivos/index.js
@@ -0,0 +1,24 @@
+// tipos primitivos
+
+// string - texto
+const nome = "João";
+
+// number
+const idade = 25;
+
+// boolean
+const possuiFaculdade = true;
+
+if (possuiFaculdade) {
+    console.log(`${nome} possui faculdade.`);
+} else {
+    console.log(`${nome} não possui faculdade.`);
+}
+
+// undefined
+let cor 
+console.log(cor); // undefined
+
+// null - nao tem valor definido
+let salario = null;
+console.log(salario); // null
