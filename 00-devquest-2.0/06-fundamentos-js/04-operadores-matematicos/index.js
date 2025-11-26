4a69c02e7d1c057be5f498097d7146e92eec2269
Gabriel <galighiti@gmail.com>
2025-11-26 10:22:22 -0300

    feat: aula sobre operadores matemáticos

---
+++ 00-devquest-2.0/06-fundamentos-js/04-operadores-matematicos/index.js
@@ -0,0 +1,24 @@
+// operadores matemáticos
+
+// soma 
+const numero1 = 10;
+const numero2 = 5;
+
+const soma = numero1 + numero2;
+console.log('Soma:', soma); // 15
+
+// subtração
+const subtracao = numero1 - numero2;
+console.log('Subtração:', subtracao); // 5
+
+// multiplicação
+const multiplicacao = numero1 * numero2;
+console.log('Multiplicação:', multiplicacao); // 50
+
+// divisão
+const divisao = numero1 / numero2;
+console.log('Divisão:', divisao); // 2
+
+// resto da divisão
+const resto = numero1 % numero2;
+console.log('Resto da Divisão:', resto); // 0
