62f32ceb6fb0130d759a1e66493f47dbc8d566bf
Gabriel <galighiti@gmail.com>
2025-11-27 16:25:18 -0300

    feat: aula sobre loops, while, do while e for

---
+++ 00-devquest-2.0/06-fundamentos-js/06-loops/for.js
@@ -0,0 +1,12 @@
+/*
+
+A sintaxe do 'for' tem 3 partes:
+**Inicializador**: onde começa a contagem (geralmente em '0')
+**Condição**: Até quando o loop deve acontecer
+**Incremento**: aumenta +1 a cada volta
+
+*/ 
+
+for (let i = 0; i <= 5; i++) {
+    console.log(`Número ${i}`);
+}
---
+++ 00-devquest-2.0/06-fundamentos-js/06-loops/while.js
@@ -0,0 +1,13 @@
+/*
+A sintaxe do 'while' tem 2 partes:
+**Condição**: Até quando o loop deve acontecer
+**Corpo**: O que deve ser repetido enquanto a condição for verdadeira
+*/
+
+let numero1 = 0;
+let numero2 = 5;
+
+while (numero1 < numero2) {
+    console.log(`Número ${numero1}`);
+    numero1++;
+}
