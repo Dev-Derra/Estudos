c780b09751b2b80d961de5a4abdb9e3d77c14882
Gabriel <galighiti@gmail.com>
2026-02-12 16:43:52 -0300

    feat: adiciona concatenação

--- 02-cursos-alura/Formação_iniciante_programação/logica-js-projeto_inicial/app.js
+++ 02-cursos-alura/Formação_iniciante_programação/logica-js-projeto_inicial/app.js
@@ -4,7 +4,7 @@ console.log(numeroSecreto);
 let chute = prompt('Digite um número entre 1 e 10:');
 
 if (numeroSecreto == chute) {
-    console.log('Parabéns! Você acertou o número secreto! (5)');
+    alert(`Parabéns! Você acertou o número secreto! ${numeroSecreto}`);
 } else {
     alert('Que pena! Você errou o número secreto');
 }
