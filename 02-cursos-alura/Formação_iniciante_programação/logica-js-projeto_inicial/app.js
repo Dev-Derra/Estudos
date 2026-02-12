c8aec6615f74ee91af91feef5b4c7d3a0ff5585b
Gabriel <galighiti@gmail.com>
2026-02-12 16:25:04 -0300

    feat:continuação da aula de lógica

--- 02-cursos-alura/Formação_iniciante_programação/logica-js-projeto_inicial/app.js
+++ 02-cursos-alura/Formação_iniciante_programação/logica-js-projeto_inicial/app.js
@@ -1,1 +1,10 @@
 alert('Boas vindas ao jogo número secreto!'); 
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute = prompt('Digite um número entre 1 e 10:');
+
+if (numeroSecreto == chute) {
+    console.log('Parabéns! Você acertou o número secreto! (5)');
+} else {
+    alert('Que pena! Você errou o número secreto');
+}
