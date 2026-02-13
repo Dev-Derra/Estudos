af69023afa13c8b5739c2c6001cbe2d7a951b467
Gabriel <galighiti@gmail.com>
2026-02-13 16:07:05 -0300

    feat: adiciona contador de tentativas

--- 02-cursos-alura/Formação_iniciante_programação/logica-js-projeto_inicial/app.js
+++ 02-cursos-alura/Formação_iniciante_programação/logica-js-projeto_inicial/app.js
@@ -1,20 +1,23 @@
 alert('Boas vindas ao jogo número secreto!');
 let numeroSecreto = 2;
 console.log(numeroSecreto);
-let chute
+let chute;
+let tentativas = 1;
+
 
 //enquanto chute não for igual ao numero secreto, o jogo continua
 while (chute != numeroSecreto) {
     chute = prompt('Digite um número entre 1 e 10:');
     // Se chute for igual ao numero secreto 
     if (chute == numeroSecreto) {
-        alert(`Parabéns! Você acertou o número secreto! ${numeroSecreto}`);
+        alert(`Parabéns! Você acertou o número secreto! ${numeroSecreto} em ${tentativas} tentativas.`);
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
+        tentativas++;
     }
 }
 
