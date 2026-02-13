bcad1e0af229a71b54c7b09030be26c2a0e6ae99
Gabriel <galighiti@gmail.com>
2026-02-13 15:44:27 -0300

    feat: adiciona novas condiçoes

--- 02-cursos-alura/Formação_iniciante_programação/logica-js-projeto_inicial/app.js
+++ 02-cursos-alura/Formação_iniciante_programação/logica-js-projeto_inicial/app.js
@@ -1,10 +1,14 @@
 alert('Boas vindas ao jogo número secreto!'); 
-let numeroSecreto = 5;
+let numeroSecreto = 2;
 console.log(numeroSecreto);
 let chute = prompt('Digite um número entre 1 e 10:');
 
-if (numeroSecreto == chute) {
+if (chute == numeroSecreto) {
     alert(`Parabéns! Você acertou o número secreto! ${numeroSecreto}`);
 } else {
-    alert('Que pena! Você errou o número secreto');
+    if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O número secreto é maior que ${chute}`);
+    }
 }
